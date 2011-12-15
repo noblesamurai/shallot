22622d0f4399cf52f99effa574779166dcdb3a44
Anneli Cuss <celtic@sairyx.org>
2011-12-15 13:56:18 +1100

    Retab, add scenario line numbers.

--- shallot.rb
+++ shallot.rb
@@ -12,174 +12,177 @@
 # A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License along with
-# shallot.  If not, see http://www.gnu.org/licenses/.
+# shallot.	If not, see http://www.gnu.org/licenses/.
 
 class Shallot
-    class Error < ::StandardError; end
+	class Error < ::StandardError; end
 
-    def self.parse f
-	parser = new
-	f.each_line {|l| parser.parse l}
-	parser.eof
+	def self.parse f
+		parser = new
+		f.each_line {|l| parser.parse l}
+		parser.eof
 
-	{
-	    feature: parser.feature,
-	    background: parser.background,
-	    scenarios: parser.scenarios,
-	}
-    end
-
-    def initialize
-	@state = :opening
-	@file_tags = []
-	@scenario_tags = []
-	@qqq = nil
-	@scenario = nil
-
-	@background = []
-	@scenarios = []
-	@feature = nil
-    end
-
-    attr_reader :background, :scenarios, :feature
-
-    def parse l
-	# Check some conditions that should be (more or less) invariant across
-	# a feature file.
-	
-	if @qqq.nil? and %w{''' """}.include? l.strip
-	    # Start triple-quote.
-	    @qqq = l.strip
-
-	elsif @qqq == l.strip
-	    # End triple-quote.
-	    @qqq = nil
-
-	elsif @qqq.nil? and [nil, ?#].include? l.strip[0]
-	    # Blank or comment; outside triple-quote.
-	    return
+		{
+			feature: parser.feature,
+			background: parser.background,
+			scenarios: parser.scenarios,
+		}
 	end
 
-	# Use state.
-	
-	method = :"parse_#@state"
-	raise Error, "no parser for #@state" unless respond_to? method
-	send method, l
-    end
+	def initialize
+		@state = :opening
+		@file_tags = []
+		@scenario_tags = []
+		@qqq = nil
+		@scenario = nil
+		@line = 0
 
-    def eof
-	method = :"eof_#@state"
-	raise Error, "no eof handler for #@state" unless respond_to? method
-	send method
-    end
-
-    protected
-
-    def parse_opening l
-	if tags = parse_tag_line(l)
-	    # Tags on beginning of file.
-	    @file_tags.concat tags
-	
-	elsif feature = parse_feature_start(l)
-	    # Start feature.
-	    @state = :feature
-	    @feature = feature
-	
-	else
-	    # There shouldn't be anything else in here.
-	    raise Error, "unexpected line before feature start"
+		@background = []
+		@scenarios = []
+		@feature = nil
 	end
-    end
 
-    def parse_feature l
-	if is_background_start? l
-	    # Start background.
-	    @state = :background
+	attr_reader :background, :scenarios, :feature
 
-	    raise Error, "tags before background" if @scenario_tags.length > 0
-	
-	elsif scenario = parse_scenario_start(l)
-	    # Start scenario (outline).
-	    start_scenario scenario
+	def parse l
+		# Check some conditions that should be (more or less) invariant across
+		# a feature file.
+		@line += 1
+		
+		if @qqq.nil? and %w{''' """}.include? l.strip
+			# Start triple-quote.
+			@qqq = l.strip
 
-	elsif tags = parse_tag_line(l)
-	    # Tags; presumably before a scenario.
-	    @scenario_tags.concat tags
+		elsif @qqq == l.strip
+			# End triple-quote.
+			@qqq = nil
 
-	else
-	    # Most likely part of the "As a .." prelude. Ignore.
+		elsif @qqq.nil? and [nil, ?#].include? l.strip[0]
+			# Blank or comment; outside triple-quote.
+			return
+		end
+
+		# Use state.
+		
+		method = :"parse_#@state"
+		raise Error, "no parser for #@state" unless respond_to? method
+		send method, l
 	end
-    end
 
-    def parse_background l
-	if @qqq.nil? and tags = parse_tag_line(l)
-	    # Tags; presumably before a scenario.
-	    @scenario_tags.concat tags
-
-	elsif @qqq.nil? and scenario = parse_scenario_start(l)
-	    # Start scenario (outline).
-	    start_scenario scenario
-	
-	else
-	    # Any other step; part of the background.
-	    @background << l.gsub(/\n$/, "")
+	def eof
+		method = :"eof_#@state"
+		raise Error, "no eof handler for #@state" unless respond_to? method
+		send method
 	end
-    end
 
-    def parse_scenario l
-	if @qqq.nil? and tags = parse_tag_line(l)
-	    # Tags; presumably before a scenario.
-	    @scenario_tags.concat tags
+	protected
 
-	elsif @qqq.nil? and scenario = parse_scenario_start(l)
-	    # Start scenario (outline).
-	    start_scenario scenario
-
-	else
-	    # Any other step; part of the scenario (outline).
-	    @scenario[:contents] << l.gsub(/\n$/, "")
+	def parse_opening l
+		if tags = parse_tag_line(l)
+			# Tags on beginning of file.
+			@file_tags.concat tags
+		
+		elsif feature = parse_feature_start(l)
+			# Start feature.
+			@state = :feature
+			@feature = feature
+		
+		else
+			# There shouldn't be anything else in here.
+			raise Error, "unexpected line before feature start"
+		end
 	end
-    end
 
-    def eof_scenario
-	@scenarios << @scenario
-    end
+	def parse_feature l
+		if is_background_start? l
+			# Start background.
+			@state = :background
 
-    def start_scenario scenario
-	@state = :scenario
-	@scenarios << @scenario if @scenario
+			raise Error, "tags before background" if @scenario_tags.length > 0
+		
+		elsif scenario = parse_scenario_start(l)
+			# Start scenario (outline).
+			start_scenario scenario
 
-	@scenario = {
-	    name: scenario[:name],
-	    outline: scenario[:outline],
-	    tags: (@file_tags + @scenario_tags).uniq,
-	    contents: []
-	}
+		elsif tags = parse_tag_line(l)
+			# Tags; presumably before a scenario.
+			@scenario_tags.concat tags
 
-	@scenario_tags = []
-    end
-
-    private
-
-    def parse_tag_line l
-	if (tags = l.strip.split).all? {|w| w[0] == ?@}
-	    tags.map {|t| t[1..-1].downcase}
+		else
+			# Most likely part of the "As a .." prelude. Ignore.
+		end
 	end
-    end
 
-    def parse_feature_start l
-	$1.strip if l.strip =~ /^feature:(.*)$/i
-    end
+	def parse_background l
+		if @qqq.nil? and tags = parse_tag_line(l)
+			# Tags; presumably before a scenario.
+			@scenario_tags.concat tags
 
-    def is_background_start? l
-	l.strip.downcase == "background:"
-    end
-
-    def parse_scenario_start l
-	if l.strip =~ /^scenario( outline)?:(.*)$/i
-	    {name: $2.strip, outline: !!$1} 
+		elsif @qqq.nil? and scenario = parse_scenario_start(l)
+			# Start scenario (outline).
+			start_scenario scenario
+		
+		else
+			# Any other step; part of the background.
+			@background << l.gsub(/\n$/, "")
+		end
 	end
-    end
+
+	def parse_scenario l
+		if @qqq.nil? and tags = parse_tag_line(l)
+			# Tags; presumably before a scenario.
+			@scenario_tags.concat tags
+
+		elsif @qqq.nil? and scenario = parse_scenario_start(l)
+			# Start scenario (outline).
+			start_scenario scenario
+
+		else
+			# Any other step; part of the scenario (outline).
+			@scenario[:contents] << l.gsub(/\n$/, "")
+		end
+	end
+
+	def eof_scenario
+		@scenarios << @scenario
+	end
+
+	def start_scenario scenario
+		@state = :scenario
+		@scenarios << @scenario if @scenario
+
+		@scenario = {
+			name: scenario[:name],
+			outline: scenario[:outline],
+			tags: (@file_tags + @scenario_tags).uniq,
+			contents: [],
+			line: @line,
+		}
+
+		@scenario_tags = []
+	end
+
+	private
+
+	def parse_tag_line l
+		if (tags = l.strip.split).all? {|w| w[0] == ?@}
+			tags.map {|t| t[1..-1].downcase}
+		end
+	end
+
+	def parse_feature_start l
+		$1.strip if l.strip =~ /^feature:(.*)$/i
+	end
+
+	def is_background_start? l
+		l.strip.downcase == "background:"
+	end
+
+	def parse_scenario_start l
+		if l.strip =~ /^scenario( outline)?:(.*)$/i
+			{name: $2.strip, outline: !!$1} 
+		end
+	end
 end
 
-# vim: set sw=4 ts=8 noet cc=80:
+# vim: set sw=4 ts=4 noet cc=80:
